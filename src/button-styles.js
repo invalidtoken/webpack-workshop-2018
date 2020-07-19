13f30db231c9ed52d4587242c91ea9158ec856ee
invalidtoken <codeit.sahil98@gmail.com>
2020-07-19 22:10:15 +0530

    basic webpack build

--- src/button-styles.js
+++ src/button-styles.js
@@ -2,6 +2,4 @@ const makeColorStyle = (colorName) => `color: ${colorName};`;
 const red = makeColorStyle("red");
 const blue = makeColorStyle("blue");
 
-exports.red = red;
-exports.blue = blue;
-exports.makeColorStyle = makeColorStyle;
+export { red, blue, makeColorStyle };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import nav from "./nav";
 import { top, bottom } from "./footer";
 
 import button from "./button"; // importing a common.js module
-import { red, blue, makeColorStyle } from "./button-styles"; // Importing a commonjs module
+import { blue, makeColorStyle } from "./button-styles"; // Importing a commonjs module
 
-console.log(nav(), top, bottom, button("Hello"));
-console.log(red, blue, makeColorStyle("green"));
+console.log(nav(), top, bottom, button("Hello"), makeColorStyle("green"), blue);
