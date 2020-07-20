9f083ae5cc29c76c69683dec6d7253474c442e4f
invalidtoken <codeit.sahil98@gmail.com>
2020-07-20 22:45:11 +0530

    adding basic webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
 module.exports = (env) => {
-  console.log(env);
   return {
     mode: env.mode,
     output: {
       filename: "bundle.js",
     },
+
+    plugins: [new HTMLWebpackPlugin(), new webpack.ProgressPlugin()],
   };
 };
